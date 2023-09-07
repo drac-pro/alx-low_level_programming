4dba9d8a29bbcc3b89e096b0ce47e2f7813baf7b
Darius Tohtin <dariustohtin@gmail.com>
2023-09-07 13:12:36 -0700

    Created script 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", (int)sizeof(char));
 
-	printf("Size of a int: %d byte(s)\n", (int)sizeof(int));
+	printf("Size of an int: %d byte(s)\n", (int)sizeof(int));
 
 	printf("Size of a long int: %d byte(s)\n", (int)sizeof(long int));
 
