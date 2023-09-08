2e1324012eedab735ad3b0a0153456bf53dd46bb
Darius Tohtin <dariustohtin@gmail.com>
2023-09-08 13:48:16 -0700

    Created c source file 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,17 +9,18 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = 1; j < 10; j++)
+		for (j = i + 1; j < 10; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
-			if ((j >= 1) && (j < 9))
-			{
-				putchar(',');
-				putchar(' ');
-			}
+
+			if (i == 8 && j == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
