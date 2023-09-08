28de5a82cef1623341c84f8d69a254fdc2acadde
Darius Tohtin <dariustohtin@gmail.com>
2023-09-08 09:06:40 -0700

    Created c source file 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	}
 	else if (ldig == 0)
 	{
-		printf("Last digit of %d is %d and is 0", n, ldig);
+		printf("Last digit of %d is %d and is 0\n", n, ldig);
 	}
 	else if (ldig < 6)
 	{
