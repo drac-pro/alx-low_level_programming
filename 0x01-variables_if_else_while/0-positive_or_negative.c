e844dcaa170fcd73662197c56a5220224f116393
Darius Tohtin <dariustohtin@gmail.com>
2023-09-08 07:45:41 -0700

    Created c source file 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d \n", n);
+	return (0);
+}
