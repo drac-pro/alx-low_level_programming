c9b3ab940368e2d675184d7e4bcbdf99f119c525
Darius Tohtin <dariustohtin@gmail.com>
2023-09-12 08:32:40 -0700

    Created c source file 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*using functions found in main.h to print letters of the alphabet */
+	print_alphabet();
+	_putchar('\n');
+
+	return (0);
+}
