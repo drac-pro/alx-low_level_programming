1d0879f21f734f3da07d1d9205174968831b134e
Darius Tohtin <dariustohtin@gmail.com>
2023-09-12 09:35:20 -0700

    Created c file 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i <=9; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+
+		_putchar('\n');
+	}
+}
