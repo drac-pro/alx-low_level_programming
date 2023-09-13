d17497fec5537d280d95271080458555e414633a
Darius Tohtin <dariustohtin@gmail.com>
2023-09-13 07:55:51 -0700

    Created c file 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (sucess))
+ */
+int main(void)
+{
+	int fib, a = 1, b = 2, i, sum;
+
+	for (i = 1;;i++)
+	{
+		if (a > 4000000)
+			break;
+		if ((a % 2) == 0)
+			sum += a;
+		fib = a + b;
+		a = b;
+		b = fib;
+	}
+
+	printf("%ld\n", sum);
+
+	return (0);
+}
