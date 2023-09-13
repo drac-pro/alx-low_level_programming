80b05e2029a50034bbd69b36743a8f1b80ae2539
Darius Tohtin <dariustohtin@gmail.com>
2023-09-12 17:58:02 -0700

    Created c file 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ *
+ * @a: number to be added
+ *
+ * @b: number to be added
+ *
+ * Return: result of addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
