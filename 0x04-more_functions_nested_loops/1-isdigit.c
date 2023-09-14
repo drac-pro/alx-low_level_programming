c4cbc91d19af1ffd11814ae42706d9bf0af79a33
Darius Tohtin <dariustohtin@gmail.com>
2023-09-14 09:27:00 -0700

    Created c file 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if character is a digit
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 if digit and 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
