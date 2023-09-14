1f4e050eff4f577ce7c8e6d48ebf30877df53692
Darius Tohtin <dariustohtin@gmail.com>
2023-09-14 12:54:22 -0700

    Created c file 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - print a square using # character
+ *
+ * @size: determines the size of the square
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+	for (j = 0; j < size; j++)
+	{
+		_putchar(35);
+	}
+	_putchar('\n');
+	}
+}
