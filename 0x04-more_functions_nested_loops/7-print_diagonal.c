d5b7f2512cfb54eb92e3314a5eceaf4d40f0a5f8
Darius Tohtin <dariustohtin@gmail.com>
2023-09-14 12:38:23 -0700

    Created c file 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,20 +15,20 @@ void print_diagonal(int n)
 		_putchar('\n');
 	else
 	{
-	for (i = 1; i <= n; i++)	
-	{
-	if (i == 1)
-        {
-                _putchar(92);
-                _putchar('\n');
-		continue;
-        }
-	for (j = 1; j <= i - 1; j++)
-	{
-		_putchar(' ');
-	}
-	_putchar(92);
-	_putchar('\n');
-	}
+		for (i = 1; i <= n; i++)
+		{
+		if (i == 1)
+		{
+			_putchar(92);
+			_putchar('\n');
+			continue;
+		}
+		for (j = 1; j <= i - 1; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		_putchar('\n');
+		}
 	}
 }
