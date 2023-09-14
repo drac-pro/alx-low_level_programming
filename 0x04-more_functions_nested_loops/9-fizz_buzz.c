86bba6117fe11238fc5d77dcaf464198f34a59f3
Darius Tohtin <dariustohtin@gmail.com>
2023-09-14 14:00:46 -0700

    Created c file 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,11 +14,20 @@ int main(void)
 	if ((i % 3 == 0) && (i % 5 != 0))
 		printf("Fizz ");
 	else if ((i % 5 == 0) && (i % 3 != 0))
+	{
+		if (i == 100)
+		{
+			printf("Buzz");
+			continue;
+		}
 		printf("Buzz ");
+	}
 	else if ((i % 3 == 0) || (i % 5 == 0))
 		printf("FizzBuzz ");
 	else
+	{
 		printf("%d ", i);
+	}
 	}
 
 	printf("\n");
