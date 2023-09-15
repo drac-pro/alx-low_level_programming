9eae48fb53f20c0b02217cce6398276c9e8685e1
Darius Tohtin <dariustohtin@gmail.com>
2023-09-14 17:32:35 -0700

    Created c file 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,11 +14,8 @@ void print_number(int n)
 		_putchar(45);
 		n = -n;
 	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
 
-	else
-	{
-		if (n > 9)
-			_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
 }
