9b2e88f390847328a024cd25deb83e504377e096
Darius Tohtin <dariustohtin@gmail.com>
2023-09-14 18:03:47 -0700

    Created c file 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,12 +9,15 @@
  */
 void print_number(int n)
 {
+	unsigned int i = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
+		i = n;
 	}
-	if (n / 10)
+	if (i / 10)
 		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	_putchar((unsigned int)(n % 10) + '0');
 }
