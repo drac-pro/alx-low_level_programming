8b3d4efdaee6edfa7a914accc5535e482acfd24e
Darius Tohtin <dariustohtin@gmail.com>
2023-09-14 18:51:55 -0700

    0x03-debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - tests function that checks positive or nagative
+*
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	printf("\n");
+
+	return (0);
+}
