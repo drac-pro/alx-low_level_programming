08a3f6c8ae7f73364fa7faa786aa8da0497c1007
Darius Tohtin <dariustohtin@gmail.com>
2023-09-18 14:29:25 -0700

    Created c file 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the later half of a string
+ * @str: pointer to the string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i, half_len, len = 0;
+
+	while (str[len] != 0)
+		len++;
+	half_len = (len - 1) / 2;
+	for (i = half_len; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
