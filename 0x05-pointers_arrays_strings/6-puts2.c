330e589afd1d299a16204e0d924aef063fc5b4e6
Darius Tohtin <dariustohtin@gmail.com>
2023-09-18 13:45:21 -0700

    Created c file 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts2 - prints every other character of a string starting from first
+ * @str: pointer to the string
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0, len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	while(i <= len - 1)
+	{
+		if (i % 2 != 0)
+			continue;
+		_putchar(str[i]);
+	}
+}
