6800f65498636a9719cbc02fbf83debc9711484e
Darius Tohtin <dariustohtin@gmail.com>
2023-09-18 12:14:59 -0700

    Created c file 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses how a string is stored
+ * @s: pointer to the string to be reversed
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len, i, j;
+	char tmp;
+
+	for (len = 0; s[0] != '\0'; len++)
+	for (i = 0, j = len - 1; i < j; i++, j--)
+	{
+		tmp = s[i];
+		s[i] = s[j];
+		s[j] = tmp;
+	}
+}
