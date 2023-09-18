2ca2d466fe58d660d89a919a3df77ce4ed5ce060
Darius Tohtin <dariustohtin@gmail.com>
2023-09-18 10:54:45 -0700

    Created c file 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - print a string to stdout
+ * @str: pointer to the string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *str != '\0'; i++)
+	{
+		_putchar(*str[i]);
+		str++;
+	}
+
+	_putchar('\n');
+}
