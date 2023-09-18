a9dd331e2469131ec7af9006a078a3103ed35854
Darius Tohtin <dariustohtin@gmail.com>
2023-09-18 13:10:51 -0700

    Created c file 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,16 +8,18 @@
  */
 void rev_string(char *s)
 {
-	int len, i;
+	int len = 0, i, j;
 	char tmp;
 
-	for (len = 0; s[0] != '\0'; len++)
-	{
-	}
-	for (i = 0; i <= len / 2; i++)
+	while (s[len] != '\0')
+		len++;
+	for (i = 0, j = len -1; i < len;)
 	{
 		tmp = s[i];
 		s[i] = s[len];
 		s[len] = tmp;
+
+		i++;
+		j--
 	}
 }
