1a7e77717ccdc4327044a57e4aa829ce075c83e7
Darius Tohtin <dariustohtin@gmail.com>
2023-09-18 16:11:35 -0700

    Created c file 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string into another string
+ * @src: pointer to source string
+ * @dest: pointer to destination string 
+ *
+ * Return: a pointer to the destination string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
