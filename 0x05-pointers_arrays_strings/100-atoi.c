baf08025de10cac0e97adafa1d5e6411dc5ae198
Darius Tohtin <dariustohtin@gmail.com>
2023-09-18 16:42:24 -0700

    Created c file 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an interger
+ * @s: pointer to the string to be converted
+ *
+ * Return: returns the integer.
+ */
+int _atoi(char *s)
+{
+	int len = 0, a = 0, i;
+
+	while (s[len] != '\0')
+		len++;
+	for (i = 0; i < len; i++)
+	{
+		a = (int)(str[i] - 48) + a * 10;
+	}
+
+	return (a);
+}
