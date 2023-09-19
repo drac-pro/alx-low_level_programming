dc4ba384df1a801acd7cd6ed28d6729159f7b6ee
Darius Tohtin <dariustohtin@gmail.com>
2023-09-19 07:27:43 -0700

    Created c file 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,25 +9,23 @@
 int _atoi(char *s)
 {
 	unsigned int num = 0;
-	int sign = 1;
-
-	while (*s == ' ')
-		s++;
-
-	if (*s == '-' || *s == '+')
-	{
-		if (*s == +)
-			continue;
-		sign *= -1;
-		s++;
-	}
+	int sign = 1, i = 1;
 
 	while (*s)
 	{
-		if (*s < '0' || *s > '9')
-			continue;
+
+	if (*s == '-')
+		sign *= -1;
+
+	while (*s >= '0' && *s <= '9')
+	{
+		i = 1
 		num = (int)(*s - 48) + num * 10;
 		s++;
+	}
+	if (i == 1)
+		break;
+	s++;
 	}
 
 	return (num * sign);
