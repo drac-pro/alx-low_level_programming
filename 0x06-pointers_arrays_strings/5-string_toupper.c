52197b0545621233900a16baedf89e333f1c53c3
Darius Tohtin <dariustohtin@gmail.com>
2023-09-20 15:45:18 -0700

    Created c file 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts lowercase letters to uppercase
+ * @s: pointer to the string
+ *
+ * Return: pointer to converted string
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] -= 32;
+	}
+}
