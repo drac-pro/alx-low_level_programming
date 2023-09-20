6815c895df71a35c84127c69d82c92ea2854f3e4
Darius Tohtin <dariustohtin@gmail.com>
2023-09-20 15:19:01 -0700

    Created c file 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (s1[i] == s2[i] && (s[i] != '\0' || s2[i] != '\0'))
+	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
 		i++;
 
 	if (s1[i] > s2[i])
