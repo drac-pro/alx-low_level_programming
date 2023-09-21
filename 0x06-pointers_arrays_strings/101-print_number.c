8f37b853c94275860bbf8dcde9cfbbbb9468e09c
Darius Tohtin <dariustohtin@gmail.com>
2023-09-20 18:36:00 -0700

    Created c file 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - print a number
+ * @n: number to be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		i = n;
+		_putchar('-');
+	}
+	i /= 10;
+	if (i != 0)
+		print_number(i);
+	_putchar((unsigned int) n % 10 + '0');
+}
