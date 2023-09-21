6022700b9e2a3bdbe8b792d779da3767da0e146a
Darius Tohtin <dariustohtin@gmail.com>
2023-09-20 17:34:50 -0700

    Created c file 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: pointer to the string to be encoded
+ *
+ * Return: pointer to encoded string
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char let[] = "aAeEoOtTlL";
+	char num[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	for (j = 0; j < 10; j++)
+	{
+		if (s[i] == let[j])
+			s[i] = num[j];
+	}
+	}
+
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,3 +13,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 
 char *cap_string(char *);
+
+char *leet(char *s);
