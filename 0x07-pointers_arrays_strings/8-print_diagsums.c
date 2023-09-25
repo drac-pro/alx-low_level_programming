fee620ffb2049a82ffd04a2df583b6bd87bf8ca8
Darius Tohtin <dariustohtin@gmail.com>
2023-09-25 16:06:51 -0700

    Created c file 8-printf_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,9 +13,21 @@ void print_diagsums(int *a, int size)
 {
 	int i, j, sum1 = 0, sum2 = 0;
 
-	for (i = 0, j = 0; i < size, j < size; i++, j++)
+	for (i = 0; i < size; i++)
+	{
+	for (j = 0; j < size; j++)
+	{
 		sum1 += a[i * size + j];
-	for (i = size - 1, j = 0; i >= 0, j < size; i--, j++)
+		break;
+	}
+	}
+	for (i = size - 1; i >= 0; i--)
+	{
+	for (j = 0; j < size; j++)
+	{
 		sum2 += a[i * size + j];
+		break;
+	}
+	}
 	printf("%d, %d\n", sum1, sum2);
 }
