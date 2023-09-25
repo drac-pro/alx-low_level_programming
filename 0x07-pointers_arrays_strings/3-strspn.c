9edcf91d06c3f4f85d7470292ae1b1f8a9fe8200
Darius Tohtin <dariustohtin@gmail.com>
2023-09-25 10:05:50 -0700

    Created c file 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,18 +11,21 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i;
-	unsigned int n = 0;
+	int i, j, n = 0;
 
-	for (i = 0; (s[i] != '\0' && accept[i] != '\0'); i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] != accept[i])
-			break;
-		else if (s[i] == accept[i])
+	for (j = 0; accept[j] != '\0'; j++)
+	{
+		if (s[i] == accept[j])
 		{
 			n++;
-			continue;
+			break;
 		}
+		break;
+	}
+	if (s[i] != accept[j])
+		return (n);
 	}
 
 	return (n);
