82cfd57f03690b4440f70942a32c1e0d14995bef
Darius Tohtin <dariustohtin@gmail.com>
2023-09-25 09:02:01 -0700

    Created c file 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - calculates the number of bytes in the initial segment of string s
+ * which consist only of bytes from another string accept
+ *
+ * @s: pointer to the first string to be calculated
+ * @accept: pointer to string containing bytes to be checked
+ *
+ * Return: number of bytes in initial section of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0' && accept[i] != '\0'; i++)
+	{
+		if (s[0] != accept[0])
+			return (0);
+		else if (s[i] == accept[i])
+			continue;
+		else
+			return (i + 1);
+	}
+}
