3150cc50e02864e42f033092e24c1e495e5a12da
Darius Tohtin <dariustohtin@gmail.com>
2023-09-28 04:29:47 -0700

    Created 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ *
+ * @s: string to be printed
+ *
+ * Return: void.
+ */
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\n')
+		i++;
+
+	/*base case*/
+	if (i == 0)
+	{
+		_putchar(s[i]);
+		_putchar(\n);
+		return;
+	}
+
+	_putchar(s[i - 1]);
+	_print_rev_recursion(s - 2);
+}
