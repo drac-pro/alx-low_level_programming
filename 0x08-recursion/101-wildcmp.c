9197240f9408d53955662d8c986d43d05e886d93
Darius Tohtin <dariustohtin@gmail.com>
2023-09-28 13:28:12 -0700

    Created 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,11 +10,17 @@
  */
 int wildcmp(char *s1, char *s2)
 {
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 	if (*s2 == '*')
+	{
+		s++;
 		wildcmp(s1, ++s2);
-	if (*s1 == *s2)
-		wildcmp(s1 + 1, s2 + 2);
-	if (*s1 != *s2)
+	}
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 		return (0);
-	return (1);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 2));
+	return (0);
 }
