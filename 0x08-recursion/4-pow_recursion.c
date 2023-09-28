951147ffce5437529e5b3e3bfb4b454465fa9d1d
Darius Tohtin <dariustohtin@gmail.com>
2023-09-28 05:27:26 -0700

    Created 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,12 +12,11 @@ int _pow_recursion(int x, int y)
 {
 	int pow = 0;
 
-	pow = x;
 	if (y < 0)
 		return (-1);
 	/*base case*/
 	if (y == 0)
-		return (1)
+		return (1);
 	if (y == 1)
 		return (x);
 	y--;
