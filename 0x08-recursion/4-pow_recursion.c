10c7df0ca3ab3884800ba077cf2a106b4cd75cb8
Darius Tohtin <dariustohtin@gmail.com>
2023-09-28 05:15:27 -0700

    Created 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculates the a number raised to a certain power
+ *
+ * @x: number
+ * @y: power to which number (x) is raised
+ *
+ * Return: result of calculation and -1 if power (y) == 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	/*base case*/
+	if (y == 1)
+		return (x);
+	y--;
+	return (_pow_recursion(x, y));
+	x *= x;
+}
