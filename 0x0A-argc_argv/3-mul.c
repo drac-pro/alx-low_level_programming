f46d911726690f5f6da11e0f4f5b66bdcc6c279e
Darius Tohtin <dariustohtin@gmail.com>
2023-10-02 11:14:52 -0700

    Created c file 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: multiplies two numbers that are passed as arguments to our
+ * program and print the result
+ *
+ * @argc: number of command line argument used when calling the program
+ * @argv: point to the arguments that where used
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+		printf("Error\n");
+	else
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
