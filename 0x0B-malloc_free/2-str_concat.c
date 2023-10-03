3089f99a3325d2a840cfe878d1b7e63c89485053
Darius Tohtin <dariustohtin@gmail.com>
2023-10-03 09:48:20 -0700

    Created c file 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,21 +15,21 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 
 	if (s1 == NULL)
-		*s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		*s2 = '\0';
+		s2 = "";
 
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 
-	size = len1 + len2 + 3;
+	size = len1 + len2 + 1;
 	ptr = malloc(sizeof(char) * size);
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < len1 + 1; i++)
+	for (i = 0; i < len1; i++)
 		ptr[i] = s1[i];
-	for (i = 0; i < len2 + 1; i++)
+	for (i = 0; i < len2; i++)
 		ptr[len1 + i] = s2[i];
 
 	ptr[size] = '\0';
