f508e1cb9fe647f203731c77f860f1ae4e60d729
Darius Tohtin <dariustohtin@gmail.com>
2023-10-04 05:25:57 -0700

    Created c file 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - cocatenates all arguments that are used when calling the program
+ *
+ * @ac: number of arguments passed
+ * @av: pointer to an array of pointer that point to the arguments
+ *
+ * Return: pointer to allocated space in memory containing concatenated strings
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, z = 0, size = 0;
+	char *_argconcat;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+		size += sizeof(av[i]);
+
+	_argconcat = malloc(size + 5);
+
+	if (_argconcat == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, z++)
+			_argconcat[z] = av[i][j];
+		_argconcat[z] = '\n';
+		z++;
+	}
+
+	return (_argconcat);
+}
