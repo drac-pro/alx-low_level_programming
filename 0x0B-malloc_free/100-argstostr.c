727fdf4e25421b1982effdc1eb8a93763e9583d4
Darius Tohtin <dariustohtin@gmail.com>
2023-10-04 06:17:12 -0700

    Created c file 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,9 +19,10 @@ char *argstostr(int ac, char **av)
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j ++)
+		for (j = 0; av[i][j] != '\0'; j++)
 			size++;
 	}
+	size += ac;
 
 	_argconcat = malloc((sizeof(char) * size) + 1);
 
