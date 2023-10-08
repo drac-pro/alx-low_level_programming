b5b32b6f1e35c9e090d112cde049db70a320bf34
Darius Tohtin <dariustohtin@gmail.com>
2023-10-08 07:38:48 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,7 +5,6 @@
 void print_num(int n);
 void print_str(char *str);
 int is_digit(char *str);
-char *remove_zero(char *str);
 /**
  * main - multiply two numbers and print the result, the numbers are passed
  * when the program is called
@@ -19,10 +18,6 @@ int main(int argc, char *argv[])
 {
 	unsigned long int num;
 
-	if (argv[1][0] == '0')
-		argv[1] = remove_zero(argv[1]);
-	if (argv[2][0] == '0')
-		argv[2] = remove_zero(argv[2]);
 	if (argc != 3)
 	{
 		print_str("Error\n");
@@ -95,17 +90,4 @@ int is_digit(char *str)
 	}
 
 	return (1);
-}
-/**
- * remove_zero - remove leading zeros from a string
- *
- * @str: pointer to the string
- *
- * Return: pointer to next non zero character
- */
-char *remove_zero(char *str)
-{
-	while (*str && *str == '0')
-		str++;
-	return (str);
 }
