ef878828c3cbb26f438536571b54cf353b8fd42c
Darius Tohtin <dariustohtin@gmail.com>
2023-10-10 10:15:46 -0700

    Created header file dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a user defined data type dog
+ *
+ * @name: pointer to name of the dog
+ * @age: age of the dog
+ * @owner:pointer to name of dogs owner
+ *
+ * Description: the dog structure holds some atributes of a real life dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG_H*/
