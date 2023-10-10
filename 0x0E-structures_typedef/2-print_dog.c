566a00974f66a35ba0cf52a7809350b45b4e68f0
Darius Tohtin <dariustohtin@gmail.com>
2023-10-10 11:32:48 -0700

    Created c file 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,6 +15,6 @@ void print_dog(struct dog *d)
 	{
 	d->name ? printf("Name: %s\n", d->name) : printf("Name: (nil)\n");
 	printf("Age: %f\n", d->age);
-	d->owner ? printf("Name: %s\n", d->owner) : printf("Name: (nil)");
+	d->owner ? printf("Owner: %s\n", d->owner) : printf("Owner: (nil)\n");
 	}
 }
