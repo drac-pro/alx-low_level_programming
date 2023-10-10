5adde83a0a8eabf20ccce3eb644616011e3e4af6
Darius Tohtin <dariustohtin@gmail.com>
2023-10-10 12:17:58 -0700

    Created c file 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,14 +12,38 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i, len1 = 0, len2 = 0;
 	dog_t *ndog;
 
 	ndog = malloc(sizeof(dog_t));
 	if (ndog == NULL)
 		return (NULL);
-	ndog->name = name;
+
+	while (name[len1])
+		len1++;
+	while (owner[len2])
+		len2++;
+
+	ndog->name = malloc(sizeof(ndog->name) * len1);
+	if (ndog->name == NULL)
+	{
+		free(ndog);
+		return (NULL);
+	}
+	for (i = 0; i < len1; i++)
+		ndog->name[i] = name[i];
+
 	ndog->age = age;
-	ndog->owner = owner;
+
+	ndog->owner = malloc(sizeof(ndog->owner) * len2);
+	if (ndog->owner == NULL)
+	{
+		free(ndog->name);
+		free(ndog);
+		return (NULL);
+	}
+	for (i = 0; i < len2; i++)
+		ndog->owner[i] == owner[i];
 
 	return (ndog);
 }
