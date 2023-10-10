45e32728f7ce4276145916eab00b9f53f45233fc
Darius Tohtin <dariustohtin@gmail.com>
2023-10-10 04:49:11 -0700

    Created c header file 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _0-OBJECT_LIKE_MACRO_H
-#define _0-OBJECT_LIKE_MACRO_H
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
-#endif /*_0-OBJECT_LIKE_MACRO_H*/
+#endif /*OBJECT_LIKE_MACRO_H*/
