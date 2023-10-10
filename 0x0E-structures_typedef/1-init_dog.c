2f93564eddd0bb36a16c17b0cfbe5f3335ed4a5d
Darius Tohtin <dariustohtin@gmail.com>
2023-10-10 10:32:00 -0700

    Created c file 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initializes the dog structure
+ *
+ * @d: pointer to a struct dog variable
+ * @name: pointer to the name string
+ * @age: pointer to the age
+ * @owner: pointer to owner string
+ *
+ * Return: void.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
