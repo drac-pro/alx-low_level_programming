c69f824f7c7978b8d1f45cdab7c94722471bf657
Darius Tohtin <dariustohtin@gmail.com>
2023-10-10 12:21:31 -0700

    Created c file 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -43,7 +43,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	for (i = 0; i < len2; i++)
-		ndog->owner[i] == owner[i];
+		ndog->owner[i] = owner[i];
 
 	return (ndog);
 }
