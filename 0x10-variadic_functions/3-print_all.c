af6e7658c9fa530afdd94cd2611f5f51a8b2b426
Darius Tohtin <dariustohtin@gmail.com>
2023-10-12 11:54:34 -0700

    Created 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,49 +1,5 @@
 #include "variadic_functions.h"
 
-void print_char(va_list all);
-void print_int(va_list all);
-void print_float(va_list all);
-void print_str(va_list all);
-
-/**
- * print_all - prints anything that is passed
- *
- * @format: const pointer to a const string which list type of arguments passed
- *
- * Return: void
- */
-void print_all(const char * const format, ...)
-{
-	char *separate = "";
-	int i = 0, n = 0;
-	va_list all;
-	type_f types[] = {
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_str},
-		{NULL, NULL}
-	};
-
-	va_start(all, format);
-	while (format[n])
-	{
-		while (types[i].type != NULL)
-		{
-			if (*types[i].type == format[n])
-			{
-				printf("%s", separate);
-				types[i].f(all);
-				separate = ", ";
-			}
-			i++;
-		}
-		i = 0, n++;
-	}
-	printf("\n");
-	va_end(all);
-}
-
 /**
  * print_char - prints a char
  *
@@ -92,4 +48,42 @@ void print_str(va_list all)
 		printf("(nil)");
 	else
 		printf("%s", str);
+}
+/**
+ * print_all - prints anything that is passed
+ *
+ * @format: const pointer to a const string which list type of arguments passed
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	char *separate = "";
+	int i = 0, n = 0;
+	va_list all;
+	type_f types[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_str},
+		{NULL, NULL}
+	};
+
+	va_start(all, format);
+	while (format && format[n])
+	{
+		while (types[i].type != NULL)
+		{
+			if (*types[i].type == format[n])
+			{
+				printf("%s", separate);
+				types[i].f(all);
+				separate = ", ";
+			}
+			i++;
+		}
+		i = 0, n++;
+	}
+	printf("\n");
+	va_end(all);
 }
