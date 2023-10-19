41e8b907a4bca35a70151836f45b12dcdfd469f3
Darius Tohtin <dariustohtin@gmail.com>
2023-10-19 11:32:37 -0700

    Created 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node - adds a neww node at the beginning of a linked list
+ *
+ * @head: pointer to the head pointer of the list
+ * @str: string to be duplicated and saved in new node
+ *
+ * Return: pointer to new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+	new_node->next = *head;
+	new_node->str = strdup(str);
+
+	return (new_node);
+}
