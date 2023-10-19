c144bb461e8e65b9632081a3b0ac63d8162b95a6
Darius Tohtin <dariustohtin@gmail.com>
2023-10-19 14:36:33 -0700

    Created 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,10 +9,13 @@
  */
 void free_list(list_t *head)
 {
+	list_t *current = head;
+
 	while (head)
 	{
+		current = head->next;
 		free(head->str);
 		free(head);
-		head = head->next;
+		head = current;
 	}
 }
