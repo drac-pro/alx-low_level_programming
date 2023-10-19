6e9cf2439c3d3af62635b15271bf8c553405e0b6
Darius Tohtin <dariustohtin@gmail.com>
2023-10-19 15:05:31 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  *
  * Return: void
  */
-__attribute__((constructor)) void before_main(void)
+void __attribute__((constructor)) before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
