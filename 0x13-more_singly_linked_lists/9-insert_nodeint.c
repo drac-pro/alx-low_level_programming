172c4494e5740b5ec66fe62027aaff1832d7f93d
Darius Tohtin <dariustohtin@gmail.com>
2023-10-23 11:30:54 -0700

    Created 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
 	for (i = 0; i < idx - 1; i++)
 	{
 		if (!current)
@@ -26,9 +36,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	if (!current)
 		return (NULL);
+
 	new_node->next = current->next;
 	current->next = new_node;
-	new_node->n = n;
 
 	return (new_node);
 }
