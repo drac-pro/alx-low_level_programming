b473b861f046e2a96f3701606a58fa225cae351d
Darius Tohtin <dariustohtin@gmail.com>
2023-10-26 07:51:10 -0700

    Created 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ *
+ * @n: unsigned number to be printed as binary
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
