a012101ba11424d64b14bb0eae001cb577aa3a80
Darius Tohtin <dariustohtin@gmail.com>
2023-10-26 11:24:05 -0700

    Edited 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,5 +11,13 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	return (__builtin_popcount(n ^ m));
+	unsigned long int diff = n ^ m;
+	unsigned int count = 0;
+
+	while (diff != 0)
+	{
+		count++;
+		diff &= diff - 1;
+	}
+	return (count);
 }
