0adad57fa6fea5fa5edae46d597b0e4225ae7163
Darius Tohtin <dariustohtin@gmail.com>
2023-10-30 17:32:34 -0700

    Created 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,26 +13,29 @@ void handle_close(int fd);
  */
 int main(int argc, char **argv)
 {
-	int fd_from, fd_to, bytes_read, bytes_written;
+	int fd_from, fd_to, bytes_read = 1024, bytes_written;
 	char buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 		handle_error("Error: Can't read from file", argv[1], 98);
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
-		| S_IRGRP | S_IWGRP | S_IROTH);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		handle_close(fd_from);
 		handle_error("Error: Error: Can't write to", argv[2], 99);
 	}
-	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
+	while (bytes_read == 1024)
 	{
+		bytes_read = read(fd_from, buffer, 1024);
+		if (bytes_read == -1)
+		{
+		handle_close(fd_from);
+		handle_close(fd_to);
+		handle_error("Error: Can't read from file", argv[1], 98);
+		}
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1 || bytes_written != bytes_read)
 		{
@@ -40,12 +43,6 @@ int main(int argc, char **argv)
 		handle_close(fd_to);
 		handle_error("Error: Error: Can't write to", argv[2], 99);
 		}
-	}
-	if (bytes_read == -1)
-	{
-		handle_close(fd_from);
-		handle_close(fd_to);
-		handle_error("Error: Can't read from file", argv[1], 98);
 	}
 	handle_close(fd_from);
 	handle_close(fd_to);
