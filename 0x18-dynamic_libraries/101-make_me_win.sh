c548bef01a0f5a3d90adf103f32bcc026ea89f84
Darius Tohtin <dariustohtin@gmail.com>
2023-12-11 08:38:19 -0800

    Edited 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/drac-pro/alx-low_level_programming/raw/master/0x18-dynamic_libraries/librand.so
-export LD_PRELOAD=/tmp/iwin.so
+wget -P /tmp https://github.com/drac-pro/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
